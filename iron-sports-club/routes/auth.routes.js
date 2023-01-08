13bcf8fbf07d42ad38689b35442d140dd8d5d763
Jan Auer <janauer2@gmx.de>
2023-01-08 20:15:41 +0800

    add GET /login und POST /login auth routes

---
+++ iron-sports-club/routes/auth.routes.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcrypt")
+const express = require("express")
+const router = express.router()
+
+const User = require("../models/User.model")
+const saltRounds = 10
+
+
+// GET Signup
+router.get("/signup" (req, res) => {
+    res.render("auth/signup")
+})
+
+
+// POST Signup
+router.post("/signup", async (req, res) => {
+    const { fullName, email, password, role } = req.body
+    console.log("REQ.BODY ===>", req.body)
+
+    //unsuccessful Signup
+    if (!fullName || !email || !password || !role) {
+        res.render("auth/signup", { errorMessage: "All fields have to be filled. Please provide your full name, email, password and role."})
+        return
+    }
+
+    //successful Signup
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    User.create({ fullName, email, password, role })
+        .then(newUser => res.redirect("/auth/profile", newUser)) //sending info of newUser
+        .catch(err => console.log(err))
+})
