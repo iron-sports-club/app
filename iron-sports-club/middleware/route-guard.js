c54192d71eafbc6cdf8bdf89541e74e995cddbca
Giulia Colombo <giulia.colombo.127@gmail.com>
2023-01-09 15:53:26 +0100

    added route guards

--- iron-sports-club/middleware/route-guard.js
+++ iron-sports-club/middleware/route-guard.js
@@ -1,7 +1,42 @@
 //isLoggedin
+const isLoggedIn = (req, res, next) => {
+    if (req.session.currentUser) {
+        next(); // execute the next action for this route
+    }
+    else {
+        return res.redirect('/auth/login');
+    }
+  
+};
 
 //isLoggedOut
+const isLoggedOut = (req, res, next) => {
+    if (!req.session.currentUser) {
+        next(); // execute the next action for this route
+    }
+    else{
+        return res.redirect('/');
+    }
+   
+  };
 
 //isInstructor
+const isInstructor = (req, res, next) => {
+    if (req.session.currentUser.role === "Instructor") {
+        next();
+}
+};
 
 //isStudent
+const isStudent = (req, res, next) => {
+    if (req.session.currentUser.role === "Student") {
+        next();
+}
+};
+
+module.exports = {
+    isLoggedIn,
+    isLoggedOut,
+    isInstructor,
+    isStudent
+  };
