9f38623bf5405e2351cf434d8a3b63e60e5c6e93
Giulia Colombo <giulia.colombo.127@gmail.com>
2023-01-10 17:14:47 +0100

    working on route guards and isOwner

--- iron-sports-club/middleware/route-guard.js
+++ iron-sports-club/middleware/route-guard.js
@@ -1,3 +1,5 @@
+const Class = require("../models/Class.model");
+
 //isLoggedin
 const isLoggedIn = (req, res, next) => {
     if (req.session.currentUser) {
@@ -34,9 +36,32 @@ const isStudent = (req, res, next) => {
 }
 };
 
+
+//isOwner > using this to display the "delete" button in the class page, for instructors that have created that class
+
+
+// const isOwner = (req, res, next) => {
+// //check if user is logged in, otherwise we'd get undefined params
+//     if (req.session.currentUser) {
+//         const {classId} = req.params; //is classId correct???
+//         const {userId} = req.session.currentUser;
+//         //get the class we want to delete
+//         //check that the owner of the found class is equal to currentUser
+//         //if it is -> next()
+//         Class.findById(classId)
+//             .then(class => {
+//                 if (class.owner === userId) {
+//                     next()
+//                 }
+//             })
+//             .then( )              
+//     }
+// };
+ 
 module.exports = {
     isLoggedIn,
     isLoggedOut,
     isInstructor,
-    isStudent
+    isStudent,
+    isOwner
   };
