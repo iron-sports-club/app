9bea1e5b56e8df2f434ac5a4ea44c1e7683b3e7b
Hadeel Abdelkareem <43421729+hadeelgamal@users.noreply.github.com>
2023-01-12 15:51:44 +0200

    updated nav styling

--- public/js/script.js
+++
@@ -1,4 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("iron-sports-club JS imported successfully!");
-});
